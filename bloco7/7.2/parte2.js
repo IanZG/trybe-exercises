398b9d69dec4a6f66b05f8ce5ff9b86656d4079d
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2020-11-30 18:35:21 -0300

    exercises dia 7.2 incompletos

---
+++ bloco7/7.2/parte2.js
@@ -0,0 +1,37 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addTurno = (obj, chave, valor) => {
+  obj[chave] = valor;
+};
+addTurno(lesson2, "turno", "manhã");
+console.log(lesson2);
+
+const listaKeys = obj => {
+  const chaves = Object.keys(obj);
+  return chaves;
+}
+console.log(listaKeys(lesson2));
+
+const tamanhoObj = obj => Object.keys(obj).length;
+console.log(tamanhoObj(lesson2));
+
+const listValues = (obj) => Object.values(obj);
+console.log(listValues(lesson1));
