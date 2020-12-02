2ee4f0fe1e33f730cba7cd8fb6cb9aecf0f89d07
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2020-12-02 00:49:45 -0300

    exercises dia 8.2

--- bloco8/8.2/exercise3.js
+++ bloco8/8.2/exercise3.js
@@ -63,10 +63,54 @@ const books = [
   },
 ];
 
-const expected_result = false;
+const expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+]
 
-function authorUnique() {
+function fantasyOrScienceFiction() {
+  // escreva seu código aqui
+  return books.filter(book => (
+    book.genre === 'Fantasia' || book.genre === 'Ficção Científica'
+  ));
+};
 
-}
-
-assert.equal(authorUnique(), expected_result);
+assert.deepEqual(fantasyOrScienceFiction(), expectedResult);
