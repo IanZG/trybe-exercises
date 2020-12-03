c4adbc08ad060b056b5bb85555a4347fce17fde3
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2020-12-03 02:37:13 -0300

    começo exercises 7.3

---
+++ bloco7/7.3/exercise.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+assert.throws(() => { sum(4, "5") });
+assert.throws(() => { sum(4, '5'); }, /^Error: parameters must be numbers$/);
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
