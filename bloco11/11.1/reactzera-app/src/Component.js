15aa3df93320d91bbd0ec54769b632cbd9381f9e
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-01-12 16:02:10 -0300

    11.1

---
+++ bloco11/11.1/reactzera-app/src/Component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Componente = <div><h1>Ian Zwanck Goodwin</h1><br /><p>Eu sou eu.</p></div> 
+class Ahiu extends React.Component {
+  render() {
+      return <h1>AhiU</h1>;
+  }
+}
+export default Ahiu;
